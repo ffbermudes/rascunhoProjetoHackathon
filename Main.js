947e79ccbec2609441d98f27cd1d15c4738b69c9
f2lpOliveira <contato@fellipefreire.com.br>
2023-03-02 06:47:19 -0300

    Redirecionar Para uma Página de Sucesso Após o Cadastro.

--- Main.js
+++ Main.js
@@ -3,9 +3,10 @@ import { adressFind } from "./public/js/adressFind.js";
 import { Conversor } from "./public/js/Conversor.js";
 import { nameCamp, lastNameCamp } from "./public/js/validateCamps.js";
 
-(()=>{
-	nameCamp(); lastNameCamp();
-})()
+(() => {
+  nameCamp();
+  lastNameCamp();
+})();
 
 //Seleciona o campo de CEP. E permite apenas números.
 const viaCepApi = new adressFind("#adressCepText");
@@ -17,9 +18,9 @@ viaCepApi.onAftereightDigits();
 
 const insertLocalStorage = new Conversor();
 
-document.querySelector("form").addEventListener("submit", e => {
-	e.preventDefault();
-	const pessoa = new Pessoa();
-	insertLocalStorage.object = pessoa;
-	insertLocalStorage.saveToLocalStorage()
+document.querySelector("form").addEventListener("submit", () => {
+  // e.preventDefault();
+  const pessoa = new Pessoa();
+  insertLocalStorage.object = pessoa;
+  insertLocalStorage.saveToLocalStorage();
 });
--- public/js/validateCamps.js
+++ public/js/validateCamps.js
@@ -1,53 +1,51 @@
-export const nameCamp = function (){
-	//Campo que será validado.
-	const campo = document.querySelector(["#nameFirstNameText"]);
-	//Div que contem a mensagem de erro.
-	const campoErro = document.querySelector("[data-msg-erro-nome]");
+const nameCamp = function () {
+  //Campo que será validado.
+  const campo = document.querySelector(["#nameFirstNameText"]);
+  //Div que contem a mensagem de erro.
+  const campoErro = document.querySelector("[data-msg-erro-nome]");
 
-	campo.addEventListener("focus", ()=>{
-		console.log("Focou no campo nome");
-	});
+  campo.addEventListener("focus", () => {
+    console.log("Focou no campo nome");
+  });
 
-	//border-3 border-danger
-	campo.addEventListener("blur", ()=>{
-		if(campo.value === "") {
-			campo.classList.remove("border-success");
-			campoErro.classList.remove("msgErroOff");
-			campo.classList.add("border-danger");
-			campo.classList.add("border-2");
-		}
-		else {
-			campoErro.classList.add("msgErroOff");
-			campo.classList.remove("border-danger");
-			campo.classList.remove("border-2");
-			campo.classList.add("border-success");
-		}
-	});
+  //border-3 border-danger
+  campo.addEventListener("blur", () => {
+    if (campo.value === "") {
+      campo.classList.remove("border-success");
+      campoErro.classList.remove("msgErroOff");
+      campo.classList.add("border-danger");
+      campo.classList.add("border-2");
+    } else {
+      campoErro.classList.add("msgErroOff");
+      campo.classList.remove("border-danger");
+      campo.classList.remove("border-2");
+      campo.classList.add("border-success");
+    }
+  });
+};
 
-}
+const lastNameCamp = function () {
+  const campo = document.querySelector(["#nameLastNameText"]);
+  const campoErro = document.querySelector("[data-msg-erro-sobrenome]");
 
-export const lastNameCamp = function (){
-	const campo = document.querySelector(["#nameLastNameText"]);
-	const campoErro = document.querySelector("[data-msg-erro-sobrenome]");
+  campo.addEventListener("focus", () => {
+    console.log("Focou no campo nome");
+  });
 
-	campo.addEventListener("focus", ()=>{
-		console.log("Focou no campo nome");
-	});
+  //border-3 border-danger
+  campo.addEventListener("blur", () => {
+    if (campo.value === "") {
+      campo.classList.remove("border-success");
+      campoErro.classList.remove("msgErroOff");
+      campo.classList.add("border-danger");
+      campo.classList.add("border-2");
+    } else {
+      campoErro.classList.add("msgErroOff");
+      campo.classList.remove("border-danger");
+      campo.classList.remove("border-2");
+      campo.classList.add("border-success");
+    }
+  });
+};
 
-	//border-3 border-danger
-	campo.addEventListener("blur", ()=>{
-		if(campo.value === "") {
-			campo.classList.remove("border-success");
-			campoErro.classList.remove("msgErroOff");
-			campo.classList.add("border-danger");
-			campo.classList.add("border-2");
-		}
-		else {
-			campoErro.classList.add("msgErroOff");
-			campo.classList.remove("border-danger");
-			campo.classList.remove("border-2");
-			campo.classList.add("border-success");
-		}
-	});
-
-}
+export { nameCamp, lastNameCamp };
